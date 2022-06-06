089528446c72f85f5889403d7670a9ccbf0cce7f
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-06-06 13:54:05 -0700

    Update wsp_het.sh

--- wsp_het.sh
+++ wsp_het.sh
@@ -9,6 +9,7 @@
 
 ml biocontainers
 ml angsd
+ml R
 
 #Path to parent directory of genus-species
 PD=/scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/
@@ -28,3 +29,17 @@ angsd -i ../final_bams/${LINE}_filt.bam -ref $REF -anc $REF -dosaf 1 -minQ 35 -G
 /depot/fnrdewoody/apps/angsd/misc/realSFS -P 20 -fold 1 ${OUTDIR}/${LINE}.saf.idx > ${OUTDIR}/${LINE}_est.ml
 
 done
+
+#Get individual SFS:
+cat ./*ml
+
+#Use Excel to calculate proportion of heterozygotes, then import file:
+
+#Import into R
+het_pupfish <- read_excel("het_pupfish.xlsx")
+#Assign factor
+het_pupfish$pop <- factor(het_pupfish$pop , levels=c("MS","SC", "LR"))
+#plot
+ggplot(het_pupfish,aes(x=pop,y=het,fill=pop))+geom_boxplot(show.legend =FALSE)+scale_fill_manual(name="pop", values =c("SC"="black","LR"="black","MS"="darkgrey"))+xlab("")+ylab("Individual Heterozygosity")+theme_classic()
+#produce plot
+ggsave("Figure_2.svg")
