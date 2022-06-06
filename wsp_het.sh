fc4c7e29f6acf10b44bc4ed0b78f8aa7b93679a7
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-06-06 13:59:19 -0700

    Update wsp_het.sh

--- wsp_het.sh
+++ wsp_het.sh
@@ -43,3 +43,42 @@ het_pupfish$pop <- factor(het_pupfish$pop , levels=c("MS","SC", "LR"))
 ggplot(het_pupfish,aes(x=pop,y=het,fill=pop))+geom_boxplot(show.legend =FALSE)+scale_fill_manual(name="pop", values =c("SC"="black","LR"="black","MS"="darkgrey"))+xlab("")+ylab("Individual Heterozygosity")+theme_classic()
 #produce plot
 ggsave("Figure_2.svg")
+
+#Test for normallity then sig
+library("ggpubr")
+library("dplyr")
+
+#How het estimates fits distribution
+ggqqplot(het_pupfish$het)
+
+#Shapiro test
+shapiro.test(het_pupfish$het)
+
+	Shapiro-Wilk normality test
+
+data:  het_pupfish$het
+W = 0.91489, p-value = 0.002849
+
+#Therefore, do non-parameteric for three pairwise tests
+wilcox.test(LR$het,SC$het)
+
+	Wilcoxon rank sum exact test
+
+data:  LR$het and SC$het
+W = 42, p-value = 0.002702
+
+wilcox.test(LR$het,MS$het)
+
+	Wilcoxon rank sum exact test
+
+data:  LR$het and MS$het
+W = 0, p-value = 1.289e-08
+
+wilcox.test(SC$het,MS$het)
+
+	Wilcoxon rank sum exact test
+
+data:  SC$het and MS$het
+W = 0, p-value = 1.289e-08
+
+
