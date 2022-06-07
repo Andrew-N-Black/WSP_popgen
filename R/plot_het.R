36eeee566d9579c2fca44357fbe885ee6be73f96
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-06-07 14:27:30 -0700

    Create plot_het.R

---
+++ R/plot_het.R
@@ -0,0 +1,45 @@
+#Use Excel to calculate proportion of heterozygotes from wsp_het.sh, then import file:
+
+#Import into R
+het_pupfish <- read_excel("het_pupfish.xlsx")
+#Assign factor
+het_pupfish$pop <- factor(het_pupfish$pop , levels=c("MS","SC", "LR"))
+#plot
+ggplot(het_pupfish,aes(x=pop,y=het,fill=pop))+geom_boxplot(show.legend =FALSE)+scale_fill_manual(name="pop", values =c("SC"="black","LR"="black","MS"="darkgrey"))+xlab("")+ylab("Individual Heterozygosity")+theme_classic()
+#produce plot
+ggsave("Figure_2.svg")
+
+#Test for normallity then sig
+library("ggpubr")
+library("dplyr")
+
+#How het estimates fits distribution
+ggqqplot(het_pupfish$het)
+
+#Shapiro test
+shapiro.test(het_pupfish$het)
+#Shapiro-Wilk normality test
+#data:  het_pupfish$het
+#W = 0.91489, p-value = 0.002849
+
+# Split into three datasets, based upon pop
+LR<-het_pupfish[1:15,]
+SC<-het_pupfish[31:45,]
+MS<-het_pupfish[16:30,]
+
+#do non-parameteric for three pairwise tests based upon wilk
+wilcox.test(LR$het,SC$het)
+
+#Wilcoxon rank sum exact test
+#data:  LR$het and SC$het
+#W = 42, p-value = 0.002702
+
+wilcox.test(LR$het,MS$het)
+#Wilcoxon rank sum exact test
+#data:  LR$het and MS$het
+#W = 0, p-value = 1.289e-08
+
+wilcox.test(SC$het,MS$het)
+#Wilcoxon rank sum exact test
+#data:  SC$het and MS$het
+#W = 0, p-value = 1.289e-08
