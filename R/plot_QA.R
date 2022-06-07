9bcb5a339c427502e0c9dde4d332fca40842f96b
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-06-07 16:55:48 -0700

    Update and rename QA_plot.R to plot_QA.R

--- R/plot_QA.R
+++ R/plot_QA.R
@@ -1,7 +1,7 @@
 #Run as Rscript to plot:
 # 1) Global coverage for all samples
-# 2) Global coverage by population
-# 3) Individual coverage, factored by population
+# 2) Individual coverage, factored by population
+# 3) Phred score distribution
 
 setWD("/scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out") 
 x<-read.table("wsp_global")
@@ -9,25 +9,6 @@ library("ggplot2")
 ggplot(data=x,aes(x=V1,y=V2))+geom_bar(stat="identity")+theme_classic()+xlab("Global Depth")+ylab("Sites")
 ggsave("All_depth.svg")
 quit()
-
-
-#Transpose and add row numbers, corresponding to cov, then plot in R
-cat MS.qa.depthGlobal | tr "\t" "\n" | awk '{print $0,NR}' | head -n 501 > MS_global
-z<-read.table("LR_global")
-ggplot(data=z,aes(x=V1,y=V2))+geom_bar(stat="identity")+theme_classic()+xlab("Global Depth MS")+ylab("Sites")
-ggsave("MS_depth.svg")
-
-
-
-y<-read.table("LR_global")
-ggplot(data=y,aes(x=V1,y=V2))+geom_bar(stat="identity")+theme_classic()+xlab("Global Depth LR")+ylab("Sites")
-ggsave("LR_depth.svg")
-
-
-zz<-read.table("LR_global")
-ggplot(data=y,aes(x=V1,y=V2))+geom_bar(stat="identity")+theme_classic()+xlab("Global Depth SC")+ylab("Sites")
-ggsave("SC_depth.svg")
-
 
 
 a<-read.table("conSamp")
@@ -42,4 +23,9 @@ b <- melt(a_sub, id.vars=c("Sample","Population"))
 neworder<-c("MS","SC","LR")
 b2<-arrange(transform(b,Population=factor(Population,levels=neworder)),Population)
 ggplot(b2, aes(x=variable,y=value,fill=as.factor(Population))) + geom_bar(stat="identity")+theme_classic()+facet_wrap(~Population,ncol=1)+scale_fill_manual( values =c("SC"="black","LR"="black","MS"="darkgrey"))+ theme(legend.position="none")+xlab("Coverage Level")+ylab("Number of Nucleotides")ggsave("Population_depth.svg")
+ggsave("Fig_S3.svg")
 
+#Phred score distribution
+phred<-read.table("ALL.qa.gs")
+ggplot(phred, aes(x=qscore,y=counts)) + geom_bar(stat="identity")+theme_classic()+ scale_x_discrete(name ="Phred score",limits=c(13:37))+ylab("Number of bases") + geom_vline(xintercept = 30,linetype="dashed")
+ggsave("Fig_S4.svg")
