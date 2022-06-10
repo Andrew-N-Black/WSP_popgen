941561b26f005fc390507703d200fd42045ca7d9
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-06-09 21:52:02 -0700

    Update wsp_poolparty.sh

--- analysis/wsp_poolparty.sh
+++ analysis/wsp_poolparty.sh
@@ -29,3 +29,9 @@ cut -f 6  pupish_analyze_raw.Sfst | cut -c 5-20 > SC.LR.fstS.txt
 cut -f 8  pupish_analyze_raw.fet  | cut -c 5-20 > LR.MS.fet.txt
 cut -f 7  pupish_analyze_raw.fet | cut -c 5-20 > SC.MS.fet.txt
 cut -f 6  pupish_analyze_raw.fet | cut -c 5-20 > SC.LR.fet.txt
+
+pupish_analyze_raw <- read.delim("~/analysis/pupish_analyze_raw.fet", header=FALSE)
+colnames(pupish_analyze_raw)<-c("scaf","bp","window","step","cov","LR:MS","SC.MS","SC.LR")
+library(qqman)
+fst.plot(pupish_analyze_raw, scaffold.widths = NULL, scaffs.to.plot = NULL,fst.name = "LR:MS", chrom.name = "scaf", bp.name = "bp", y.lim = NULL, xlabels = NULL, xlab.indices = NULL, axis.size = 0.5, pt.cols = c("darkgrey", "lightgrey"),pt.cex = 0.5)
+
