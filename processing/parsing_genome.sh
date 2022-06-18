b3fb715ecd1c578ccc0c6895bc7e5453734b1d16
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-06-18 10:59:17 -0700

    Create parsing_genome.sh

---
+++ processing/parsing_genome.sh
@@ -0,0 +1,17 @@
+ml biocontainers
+ml bedtools
+
+#Use bedtools to create coordinates of exons
+awk 'BEGIN{OFS="\t";} $3=="exon" {print $1,$4-1,$5}' GCF_016077235.1.gtf |
+ bedtools sort | bedtools merge -i - > tularosa_exon.bed
+
+
+
+#Use bedtools to create coordinates of intergenic areas
+cat GCF_016077235.1.gtf |
+  awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4-1,$5}' |
+  bedtools sort -g chrom |
+  bedtools complement -i stdin -g chrom > intergenic.bed
+  gzip > my_intergenic.bed.gz
+  
+  
