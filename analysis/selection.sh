8a57ab4f65d60aa38ee968e3e9dfa31b222743c9
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-06-21 15:15:28 -0700

    Update selection.sh

--- analysis/selection.sh
+++ analysis/selection.sh
@@ -1,28 +1,28 @@
- 
- qqchi<-function(x,...){
-+ lambda<-round(median(x)/qchisq(0.5,1),2)
-+   qqplot(qchisq((1:length(x)-0.5)/(length(x)),1),x,ylab="Observed",xlab="Expected",...);abline(0,1,col=2,lwd=2)
-+ legend("topleft",paste("lambda=",lambda))
-+ }
- 
-s<-npyLoad("exons.selection.npy")
+qqchi<-function(x,...){
+lambda<-round(median(x)/qchisq(0.5,1),2)
+  qqplot(qchisq((1:length(x)-0.5)/(length(x)),1),x,ylab="Observed",xlab="Expected",...);abline(0,1,col=2,lwd=2)
+legend("topleft",paste("lambda=",lambda))
+}
+
+library(RcppCNPy)
+s<-npyLoad("EXON.selection.npy")
 qqchi(s)
 pval<-1-pchisq(s,1)
 
-p<-read.table("exon.sites",colC=c("factor","integer"),sep="\t")
+# awk '{print $1}' EXON.beagle  | tail -n +2 | sed 's/NW_/NW./g' |sed 's/_/\t/g' |sed 's/NW./NW_/g' > EXON.sites
+p<-read.table("EXON.sites",colC=c("factor","integer"),sep="\t")
 names(p)<-c("chr","pos")
  p$pos[which.max(s)]
-[1] 245807
+[1] 447209
 
 exon_df<-cbind(p,s,pval)
 plot(-log10(pval),col=p$chr,xlab="Chromosomes",main="Manhattan plot")
-exons<--log10(exon_dataframe$pval)
-qplot(exons,
-+       geom="histogram",
-+       binwidth = 0.1,  
-+       main = "Exon selection", 
-+       xlab = "-log10(p-value)",  
-+       fill=I("grey"), 
-+       col=I("black"))+theme_bw()
+exons<--log10(exon_df$pval)
+library(qvalue)
+qval <- qvalue(exon_df$pval)$qvalues
+EXON_DF<-cbind(exon_df,qval)
+
+qplot(-log10(exon_df$pval), geom="density",main = "Exon selection",xlab = "-log10(p-value)",fill=I("grey"),col=I("black"))+theme_bw()+geom_vline(xintercept =1.30103,linetype="dashed")
+
 
 
