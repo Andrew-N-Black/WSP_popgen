d9f1df2a1357356d144b2704211f73173b37a614
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-07-02 14:06:22 -0700

    Update wsp_pca.sh

--- analysis/wsp_pca.sh
+++ analysis/wsp_pca.sh
@@ -1,30 +1,71 @@
 #!/bin/sh -l
 #SBATCH -A fnrquail
 #SBATCH -N 1
-#SBATCH -n 100
-#SBATCH -t 10-00:00:00
+#SBATCH -n 50
+#SBATCH -t 1-00:00:00
 #SBATCH --job-name=angsd_pca
 #SBATCH -e %x_%j.err
 #SBATCH -o %x_%j.out
-#SBATCH --mem=100GB
+#SBATCH --mem=50GB
 
 
 module load biocontainers
 module load pcangsd
 module load angsd
-module load R
 
 
 REF=/scratch/bell/blackan/PUPFISH/C.tularosa/assembly/ncbi/Cyprinodon-tularosa/GCF_016077235.1_ref/ref.fa
 
-#Generate angsd output files (beagle and mags)
-angsd -P 100 -out /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pupfish_final \
--minInd 36 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -C 50 -baq 1 -minMapQ 30 -minQ 35 -setMinDepth 275 -setMaxDepth 425 \
--bam ./ALL_bamlist_norealign -doCounts 1 -GL 1 -doGlf 2 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -anc $REF -ref $REF
-
 
 #Run PCA
-pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pupfish_final.beagle.gz --minMaf 0.05 -o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/pupfish_final --threads 100 --sites_save --maf_tole 1e-9 --tole 1e-9 --iter 5000 --maf_iter 5000
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz --selection --tree \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/FINAL --threads 50 --pcadapt --inbreedSamples
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/EXON.beagle.gz --selection --tree \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/FINAL --threads 50 --pcadapt --inbreedSamples
+
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/INTER.beagle.gz --selection --tree \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/FINAL --threads 50 --pcadapt --inbreedSamples
+
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/FINAL --threads 50 --admix -e 1  --admix_iter 100 --admix_K 2 --minMaf 0.05
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/FINAL --threads 50 --admix -e 1  --admix_iter 100 --admix_K 3 --minMaf 0.05
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/FINAL --threads 50 --admix -e 1  --admix_iter 100 --admix_K 4 --minMaf 0.05
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/FINAL --threads 50 --admix -e 1  --admix_iter 100 --admix_K 5 --minMaf 0.05
+
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/EXON.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/EXON --threads 50 --admix -e 1  --admix_iter 100 --admix_K 2 --minMaf 0.05
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/EXON.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/EXON --threads 50 --admix -e 1  --admix_iter 100 --admix_K 3 --minMaf 0.05
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/EXON.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/EXON --threads 50 --admix -e 1  --admix_iter 100 --admix_K 4 --minMaf 0.05
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/EXON.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/EXON --threads 50 --admix -e 1  --admix_iter 100 --admix_K 5 --minMaf 0.05
+
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/INTER.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/INTER --threads 50 --admix -e 1  --admix_iter 100 --admix_K 2 --minMaf 0.05
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/INTER.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/INTER --threads 50 --admix -e 1  --admix_iter 100 --admix_K 3 --minMaf 0.05
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/INTER.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/INTER --threads 50 --admix -e 1  --admix_iter 100 --admix_K 4 --minMaf 0.05
+
+pcangsd -b /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/INTER.beagle.gz \
+-o /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pca/INTER --threads 50 --admix -e 1  --admix_iter 100 --admix_K 5 --minMaf 0.05
 
 #Plot
 setwd("./")
