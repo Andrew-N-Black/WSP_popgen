09ae878829ba67b01f5fded38874827d95f3b8c0
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-07-09 13:11:33 -0700

    Update pca_plot.R

--- R/poolseq/pca_plot.R
+++ R/poolseq/pca_plot.R
@@ -16,9 +16,9 @@ head(popmap)
 
 #Pool-seq pca 
 library(data.table)
-infile<-fread(file="~/analysis/pupfish.fz", showProgress=FALSE, header=FALSE)
-colnames(infile) <- c("Chr","Pos","Ref","Alt", "SC","LR","MS")
+infile<-fread(file="~/analysis/pupfish.fz", showProgress=FALSE, header=TRUE)
 X<-infile[,c(5:7)]
+colnames(X) <- c("SC","LR","MS")
 pca1<-prcomp(na.omit(X),scale. = TRUE)
 #Calculate variance explained
 pca.eig= pca1$sdev^2
@@ -28,7 +28,7 @@ ax1 <- paste ("PC1 ","(", ax1,"%",")", sep= "")
 ax2 <- paste ("PC2 ","(", ax2,"%",")", sep= "")
 
 plotting = as.data.frame(pca1$rotation[,1:3])
-ESU<-c("ESU-1","ESU-1,"ESU-2")
+ESU<-c("ESU-1","ESU-1","ESU-2")
 pop<-c("SC","LR","MS")
 ggplot(plotting, aes(x=PC1,y=PC2,color=ESU, shape=as.factor(pop)))+geom_point(size=5)+scale_shape_manual(title,values=c(20,18,4))+ scale_color_manual(name="", values =c("ESU-1"="darkgrey","ESU-2"="black"))+theme(legend.position = "none",panel.grid = element_blank())+theme_classic()+xlab(ax1) +ylab(ax2)+ylim(c(0,1))+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+ggtitle("Pool-seq")+ggtitle("A")
 
