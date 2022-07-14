48151757accb7cf70a26307951def330fe17c0f9
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-07-14 11:39:57 -0700

    Update wsp_admix.sh

--- analysis/lcWGR/wsp_admix.sh
+++ analysis/lcWGR/wsp_admix.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
-#SBATCH -A fnrtowhee
+#SBATCH -A fnrchook
 #SBATCH -N 1
 #SBATCH -n 64
 #SBATCH -t 12-00:00:00
-#SBATCH --job-name=admix_all
+#SBATCH --job-name=admix_parse.05
 #SBATCH --error=%x_%j.out
 #SBATCH --output=%x_%j.out
 #SBATCH --mem=100G
@@ -12,17 +12,30 @@ module purge
 module load bioinfo
 
 
-j="1"
-while [ $j -lt 20 ]
-do
-	for i in {1..7}
-	do
-                 /depot/fnrdewoody/apps/angsd/misc/NGSadmix -P 64  \
-                -K ${i} -minMaf 0.05 -maxiter 50000 -tol 1e-9 -tolLike50 1e-9 \
-                -likes /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/pupfish_filtered.beagle.gz \
-                -outfiles  /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/adm/r${j}/filtered_k${i}
-                i=$[$i+1]
+/depot/fnrdewoody/apps/angsd/misc/NGSadmix -P 64  \
+                -K 1 -minMaf 0.05 -maxiter 50000 -tol 1e-9 -tolLike50 1e-9 \
+                -likes /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz \
+                -outfiles  /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/ADX/FINAL_K1
+          
 
-	done
-	j=$[$j+1]
-done
+/depot/fnrdewoody/apps/angsd/misc/NGSadmix -P 64  \
+                -K 2 -minMaf 0.05 -maxiter 50000 -tol 1e-9 -tolLike50 1e-9 \
+                -likes /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz \
+                -outfiles  /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/ADX/FINAL_K2  
+
+/depot/fnrdewoody/apps/angsd/misc/NGSadmix -P 64  \
+                -K 3 -minMaf 0.05 -maxiter 50000 -tol 1e-9 -tolLike50 1e-9 \
+                -likes /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz \
+                -outfiles  /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/ADX/FINAL_K3  
+
+/depot/fnrdewoody/apps/angsd/misc/NGSadmix -P 64  \
+                -K 4 -minMaf 0.05 -maxiter 50000 -tol 1e-9 -tolLike50 1e-9 \
+                -likes /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz \
+                -outfiles  /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/ADX/FINAL_K4  
+
+/depot/fnrdewoody/apps/angsd/misc/NGSadmix -P 64  \
+                -K 5 -minMaf 0.05 -maxiter 50000 -tol 1e-9 -tolLike50 1e-9 \
+                -likes /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/FINAL.beagle.gz \
+                -outfiles  /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/ADX/FINAL_K5  
+		
+#DONE
