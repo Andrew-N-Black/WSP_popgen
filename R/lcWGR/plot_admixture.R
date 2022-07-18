769210fa927e7ca1e73a9ea272a1ec7dacf6165f
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-07-18 15:53:42 -0700

    Update plot_admixture.R

--- R/lcWGR/plot_admixture.R
+++ R/lcWGR/plot_admixture.R
@@ -1,11 +1,13 @@
-
+#Plot Q matrx (Figure 2)
 library(pophelper)
-
-k2<-readQ("~/ADX/FINAL_K2.qopt")
-k3<-readQ("~/ADX/FINAL_K3.qopt")
+setwd("~/files")
+k2<-readQ("FINAL_K2.qopt")
+k3<-readQ("FINAL_K3.qopt")
 
 plotQ(qlist=k2,returnplot=T,exportplot=F,basesize=11,showindlab=F, clustercol=c("black","darkgrey"))
-ggsave("k2.svg")
+ggsave("Fig_2A.svg")
 
 plotQ(qlist=k3,returnplot=T,exportplot=F,basesize=11,showindlab=F, clustercol=c("slateblue","black","darkgrey"))
-ggsave("k3.svg")
+ggsave("Fig_2B.svg")
+
+#Done
