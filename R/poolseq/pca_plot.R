6f41e769d156be1986f59f5dc28891327b3103aa
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2022-08-03 15:33:41 -0700

    Update pca_plot.R

--- R/poolseq/pca_plot.R
+++ R/poolseq/pca_plot.R
@@ -1,4 +1,7 @@
 #Plot pool-seq PCA
+#R version 4.2.0
+#ggplot2 version 3.3.6 
+
 library(ggplot)
 #Read in metadata
 popmap <- read.delim("popmap", header=FALSE)
@@ -33,5 +36,5 @@ pop<-c("SC","LR","MS")
 ggplot(plotting, aes(x=PC1,y=PC2,color=ESU, shape=as.factor(pop)))+geom_point(size=5)+scale_shape_manual(title,values=c(20,18,4))+ scale_color_manual(name="", values =c("ESU-1"="darkgrey","ESU-2"="black"))+theme(legend.position = "none",panel.grid = element_blank())+theme_classic()+xlab(ax1) +ylab(ax2)+ylim(c(0,1))+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed")+ggtitle("Pool-seq")+ggtitle("A")
 
 ggsave("Fig_1A.svg")
-
+#DONE
 
