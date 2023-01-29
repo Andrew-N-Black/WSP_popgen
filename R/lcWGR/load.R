b19ef4229ea461694d8c86b154b51f64120e3ca6
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-01-29 12:04:23 -0800

    Create load.R

---
+++ R/lcWGR/load.R
@@ -0,0 +1,8 @@
+library(ggplot2)
+library(reshape2)
+library(readxl)
+load <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/WS_Pupfish/PopulationGenomics/Manuscript/Figures/Files/load.xlsx")
+b <- melt(load, id.vars=c("Pop","ESU"))
+
+ggplot(b, aes(x=Pop,y=value,fill=as.factor(ESU))) + geom_bar(stat="identity")+theme_classic()+facet_wrap(~variable,ncol=4,scales = "free_y")+scale_fill_manual("", values =c("ESU-1"="black","ESU-2"="blue"))+xlab("")+ylab("Load Proportion")+ theme(axis.text.x = element_text(size = 10))+ theme(axis.text.x = element_text(angle = 45, vjust = 0.95, hjust=1))
+ggsave("~/wsp_load.svg")
