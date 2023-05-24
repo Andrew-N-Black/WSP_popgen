2714fd7c7d711b45c746fbe29e1c2b308a6137bd
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-05-23 22:18:55 -0700

    Create divergence_plot.R

---
+++ R/poolseq/divergence_plot.R
@@ -0,0 +1,3 @@
+MS_SC_FST_union <- read_excel("pupfish_fst/MS.SC.FST_union.xlsx")
+ggplot(MS_SC_FST_union, aes(x=contig_base_pool, y=fst_pool)) +geom_point(size=1,shape=1)+ theme_classic() + xlab("Contig")+ylab("Fst")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+xlab("Contig")
+ggsave("~/sc-ms-pool.svg")
