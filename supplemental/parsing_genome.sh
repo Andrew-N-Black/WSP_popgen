fe2b93a6018b21c41c073644ab304aac022fef93
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-05-27 09:13:09 -0700

    Update parsing_genome.sh

--- supplemental/parsing_genome.sh
+++ supplemental/parsing_genome.sh
@@ -8,12 +8,10 @@ awk 'BEGIN{OFS="\t";} $3=="exon" {print $1,$4-1,$5}' GCF_016077235.1.gtf |
 
 
 #Use bedtools to create coordinates of intergenic areas
-cat GCF_016077235.1.gtf |
-  awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4-1,$5}' |
-  bedtools sort -g chrom |
-  bedtools complement -i stdin -g chrom > intergenic.bed
-  gzip > my_intergenic.bed.gz
-  
+samtools faidx GCF_016077235.1_ASM1607723v1_genomic.fna 
+cut -f 1-2 GCF_016077235.1_ASM1607723v1_genomic.fna.fai > chrom
+
+cat GCF_016077235.1_ASM1607723v1_genomic.gtf | awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4-1,$5}' |   bedtools sort -g chrom |   bedtools complement -i stdin -g chrom > intergenic.bed
   
 awk '{print $1}' exon.beagle | sed 's/NW_/NW./g'| tr "1_" "1\t" | sed 's/NW./NW_/g' > ../exons.bed
 
