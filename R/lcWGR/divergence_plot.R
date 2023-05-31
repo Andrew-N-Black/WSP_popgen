90c8226e4c6d622926b46b724ebe127d32bf16d0
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-05-30 18:11:48 -0700

    Update divergence_plot.R

--- R/lcWGR/divergence_plot.R
+++ R/lcWGR/divergence_plot.R
@@ -2,3 +2,11 @@ MS_SC_FST_union <- read_excel("pupfish_fst/MS.SC.FST_union.xlsx")
 ggplot(MS_SC_FST_union, aes(x=contig_base_wgr, y=fst_wgr)) +geom_point(size=1,shape=1)+ theme_classic() + xlab("Contig")+ylab("Fst")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+xlab("Contig")
 
 ggsave("~/sc-ms-wgr.svg")
+
+
+
+
+
+ggplot(MATCH, aes(x=`Contig-bp`, y=fst_pool)) +geom_point(size=1,shape=1)+ theme_classic() + xlab("Contig")+ylab("Fst")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+ggtitle("Pool-seq-Genic")
+ggplot(MATCH, aes(x=`Contig-bp`, y=fst_wgr_un)) +geom_point(size=1,shape=1)+ theme_classic() + xlab("Contig")+ylab("Fst")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+ggtitle("Pool-seq-Genic")
+
