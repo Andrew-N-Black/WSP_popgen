360f5b1d1cd4d26868fc17e837d0e63afa985341
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-05 10:43:25 -0700

    Update ROH.sh

--- analysis/lcWGR/ROH.sh
+++ analysis/lcWGR/ROH.sh
@@ -6,18 +6,25 @@
 #SBATCH --error=bcf.e
 #SBATCH --output=bcf.o
 #SBATCH --mem=100G
+
+#Load modules
 module load bioinfo
 module load bcftools
 module load samtools
-#Move to bam folder
 
-#angsd -GL 1 -dobcf 1 -dopost 1 -domajorminor 1 -domaf 1 -minQ 30 -P 64 \
-#-baq 2 -SNP_pval 1e-6 -bam bamlist.txt -ref /scratch/bell/blackan/PUPFISH/C.tularosa/assembly/ncbi/Cyprinodon-tularosa/GCF_016077235.1_ref/ref_100k.fa
+#Move to bam folder and generate a bcf
+angsd -GL 1 -dobcf 1 -dopost 1 -domajorminor 1 -domaf 1 -minQ 30 -P 64 \
+-baq 2 -SNP_pval 1e-6 -bam bamlist.txt -ref /scratch/bell/blackan/PUPFISH/C.tularosa/assembly/ncbi/Cyprinodon-tularosa/GCF_016077235.1_ref/ref_100k.fa
 
+#Create tab freq file
 bcftools query -f'%CHROM\t%POS\t%REF,%ALT\t%INFO/AF\n' angsdput.bcf | bgzip -c > PUP.freqs.tab.gz
 tabix -s1 -b2 -e2 PUP.freqs.tab.gz
+#Generate ROH file
 bcftools roh --AF-file PUP.freqs.tab.gz --output ROH_PUP_PLraw.txt --threads 64 angsdput.bcf
 
+#Extract RG from raw calls
 awk '$1=="RG"' ROH_PUP_PLraw.txt > ROH_RG_all.txt
+
+#Parse file into indivudals
 for i in `cat sample.names`; do  grep $i ROH_RG_all.txt  > $i.ROH.txt ; done
 for i in `ls -1 *ROH.txt`; do python ROHparser.py $i > ${i}_results.txt ; done
