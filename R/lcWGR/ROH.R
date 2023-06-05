c268c959a62432cb4399fcd59f5d7c4bdb3e9ff4
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-05 10:17:16 -0700

    Update ROH.R

--- R/lcWGR/ROH.R
+++ R/lcWGR/ROH.R
@@ -1,8 +1,11 @@
-ind_rohs <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/WS_Pupfish/PopulationGenomics/Manuscript/Figures/Files/ind_rohs.xlsx")
+
+
+setwd("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/WS_Pupfish/PopulationGenomics/Manuscript/Figures/Files/")
+ind_rohs <- read_excel("ind_rohs.xlsx")
 ind_rohs<-as.data.frame(ind_rohs)
 melt_data<-melt(ind_rohs,id.vars = c("ID","Pop","ESU"))
 melt_data$Pop <- factor(melt_data$Pop, levels = c("Malpais Spring","Salt Creek","Lost River"))
 ggplot(melt_data, aes(fill=Pop, y=value, x=reorder(ID,value))) +geom_bar(stat="identity",position="dodge")+facet_grid(variable~Pop,scales = "free")+theme(axis.title.x=element_blank(),axis.text.x=element_blank(),axis.ticks.x=element_blank())+theme_classic()+
 +     scale_fill_manual(values=c("black","black","blue"))+ylab("fROH")+xlab("Sample (N=45)")+
 +     theme(legend.position="none")+theme(axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
-ggsave("~/Figure_1d.svg")
+
