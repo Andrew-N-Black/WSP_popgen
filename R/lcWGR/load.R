1b8682870fb3272aecea76c25eb5cc6037a615b9
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-05 10:34:45 -0700

    Update load.R

--- R/lcWGR/load.R
+++ R/lcWGR/load.R
@@ -1,13 +1,17 @@
+#R version 4.2.0
 library(ggplot2)
 library(reshape2)
 library(readxl)
-load <- read_excel("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/WS_Pupfish/PopulationGenomics/Manuscript/Figures/Files/load.xlsx")
+setwd("Library/CloudStorage/Box-Box/Personal/Postdoc_Purdue/WS_Pupfish/PopulationGenomics/Manuscript/Figures/Files/")
+
+#Read in excel file
+load <- read_excel("load.xlsx")
+#Set order of factors
 load$Pop <- ordered(load$Pop,levels = c("LR","SC","MS"))
+#Reshape dataframe
 b <- melt(load, id.vars=c("Pop","ESU"))
-
+#plot
 ggplot(b, aes(x=Pop,y=value,fill=as.factor(ESU))) + geom_bar(stat="identity")+theme_classic()+facet_wrap(~variable,ncol=4,scales = "free_y")+scale_fill_manual("", values =c("ESU-1"="black","ESU-2"="blue"))+xlab("")+ylab("Load Proportion")+ theme(axis.text.x = element_text(size = 10))+ theme(axis.text.x = element_text(angle = 45, vjust = 0.95, hjust=1))
-ggsave("~/wsp_load.svg")
-
 
 #Venn diagram of high sites
 library(readr)
