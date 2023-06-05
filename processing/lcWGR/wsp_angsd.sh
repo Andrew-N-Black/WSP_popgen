d741db8369b9d1a490d32fc6a50c7978ab0bace7
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-05 11:50:57 -0700

    Update wsp_angsd.sh

--- processing/lcWGR/wsp_angsd.sh
+++ processing/lcWGR/wsp_angsd.sh
@@ -25,38 +25,6 @@ gzip -d FINAL.beagle.gz
 awk '{print $1}' FINAL.beagle | sed 's/NW_/NW./g' | sed 's/_/\t/g' | sed 's/NW./NW_/g' | tail -n +2 > FINAL.SITES
 gzip FINAL.beagle.gz
 
-#angsd sites index exon.bed 
-
-#Generate angsd output files for exon regions
-angsd -P 80 -out /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/EXON \
--bam ./ALL_bamlist -doCounts 1 -GL 1 -doGlf 2 -doMajorMinor 4 -doMaf 1 -minMaf 0.05 -rmTriallelic  1  \
--minInd 36 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -C 50 -baq 1 -minMapQ 30 -minQ 35 -setMinDepth 275 -setMaxDepth 425 -SNP_pval 1e-6  -ref $REF -anc $REF \
--sites exon.bed -rf chr.exon
-
-
-
-#Get list of sites:
-gzip -d EXON.beagle.gz
-awk '{print $1}' EXON.beagle | sed 's/NW_/NW./g' | sed 's/_/\t/g' | sed 's/NW./NW_/g' | tail -n +2 > EXON.SITES
-gzip EXON.beagle
-
-
-#Index intergenic sites
-#angsd sites index inter.bed
-
-#Generate angsd output files for intergenic regions
-angsd -P 80 -out /scratch/bell/blackan/PUPFISH/C.tularosa/popgen/illumina/angsd_out/INTER \
--bam ./ALL_bamlist -doCounts 1 -GL 1 -doGlf 2 -doMajorMinor 4 -doMaf 1 -minMaf 0.05 -rmTriallelic 1  \
--minInd 36 -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 1 -C 50 -baq 1 -minMapQ 30 -minQ 35 -setMinDepth 275 -setMaxDepth 425 -SNP_pval 1e-6  -ref $REF -anc $REF \
--sites  inter.bed -rf chr.inter
-
-
-
-#Get list of sites:
-gzip -d INTER.beagle.gz
-awk '{print $1}' INTER.beagle | sed 's/NW_/NW./g' | sed 's/_/\t/g' | sed 's/NW./NW_/g' | tail -n +2 > INTER.SITES
-gzip INTER.beagle
-
 
 
 
