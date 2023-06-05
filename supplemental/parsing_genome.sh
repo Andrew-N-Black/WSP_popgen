d7447675ed781c61df0707d601ce191726470f3e
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2023-06-05 11:58:55 -0700

    Update parsing_genome.sh

--- supplemental/parsing_genome.sh
+++ supplemental/parsing_genome.sh
@@ -1,18 +1,15 @@
 ml biocontainers
 ml bedtools
 
-#Use bedtools to create coordinates of exons
-awk 'BEGIN{OFS="\t";} $3=="exon" {print $1,$4-1,$5}' GCF_016077235.1.gtf |
- bedtools sort | bedtools merge -i - > tularosa_exon.bed
-
-
-
 #Use bedtools to create coordinates of intergenic areas
 samtools faidx GCF_016077235.1_ASM1607723v1_genomic.fna 
 cut -f 1-2 GCF_016077235.1_ASM1607723v1_genomic.fna.fai > chrom
 
+#Use bedtools to create coordinates of genes
+awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4-1,$5}' GCF_016077235.1.gtf |
+ bedtools sort -g chrom| bedtools merge -i - > genes.bed
+
+#Get coordinates of intergenic regions
 cat GCF_016077235.1_ASM1607723v1_genomic.gtf | awk 'BEGIN{OFS="\t";} $3=="gene" {print $1,$4-1,$5}' |   bedtools sort -g chrom |   bedtools complement -i stdin -g chrom > intergenic.bed
-  
-awk '{print $1}' exon.beagle | sed 's/NW_/NW./g'| tr "1_" "1\t" | sed 's/NW./NW_/g' > ../exons.bed
 
 #DONE
