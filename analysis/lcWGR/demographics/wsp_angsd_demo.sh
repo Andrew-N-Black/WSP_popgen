23594730a95b63bd7240db2d3f0bea4cad9891d6
Samarth Mathur <mathur20@purdue.edu>
2024-03-17 15:52:20 -0400

    fix: updated demographic scripts with final version

--- analysis/lcWGR/demographics/wsp_angsd_demo.sh
+++ analysis/lcWGR/demographics/wsp_angsd_demo.sh
@@ -2,9 +2,9 @@
 #SBATCH -A highmem
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=100G
+#SBATCH --mem=500G
 #SBATCH -t 1-00:00:00
-#SBATCH --job-name=2Dsfs
+#SBATCH --job-name=3Dsfs
 #SBATCH -e %x
 #SBATCH -o %x
 
@@ -12,20 +12,19 @@
 ###                          Samarth Mathur, PhD                     	###
 ###                        The Ohio State University                 	###
 ###                                                                     ###
-###     Date Created: 09/27/22                  Last Modified: 11/11/22 ###
 ###########################################################################
 ###########################################################################
-###                     angsd.sh                        				###
+###                     wsp_angsd_demo.sh                        		###
 ###########################################################################
 
 cd $SLURM_SUBMIT_DIR
-module load biocontainers/default
+module load bioinfo
 module load bwa
 module load samtools
 
 # Get genotype likelihood of all samples
 
-cd /scratch/bell/mathur20/pupfish/angsd/gl/
+#cd /scratch/bell/mathur20/pupfish/angsd/gl/
 
 #/depot/fnrdewoody/apps/angsd/angsd -P 128 \
 #-doCounts 1 -GL 1 -doGlf 2 -doMajorMinor 4 -doMaf 1 -minMaf 0.05 -rmTriallelic 1  \
@@ -53,7 +52,34 @@ cd /scratch/bell/mathur20/pupfish/angsd/gl/
 
 # Get 1D SFS for all samples and each population (1. LR_ESU1 2. SC_ESU1 3. MS_ESU2) separately
 
-#cd /scratch/bell/mathur20/pupfish/angsd/sfs
+cd /scratch/bell/mathur20/pupfish/angsd/sfs
+
+#/depot/fnrdewoody/apps/angsd/angsd -P 128 \
+#-dosaf 1 -minQ 35 -GL 1 \
+#-anc /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
+#-bam /scratch/bell/mathur20/pupfish/lists/LR_ESU1.bamlist.txt \
+#-out LR_ESU1 
+
+#/depot/fnrdewoody/apps/angsd/angsd -P 128 \
+#-dosaf 1 -minQ 35 -GL 1 \
+#-anc /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
+#-bam /scratch/bell/mathur20/pupfish/lists/SC_ESU1.bamlist.txt \
+#-out SC_ESU1 
+
+
+#/depot/fnrdewoody/apps/angsd/angsd -P 128 \
+#-dosaf 1 -minQ 35 -GL 1 \
+#-anc /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
+#-bam /scratch/bell/mathur20/pupfish/lists/ESU2.bamlist.txt \
+#-out MS_ESU2 
+
+#/depot/fnrdewoody/apps/angsd/angsd -P 128 \
+#-dosaf 1 -minQ 35 -GL 1 \
+#-anc /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
+#-bam /scratch/bell/mathur20/pupfish/lists/ESU1.bamlist.txt \
+#-out ESU1 
+
+# all samples together #
 
 #/depot/fnrdewoody/apps/angsd/angsd -P 128 \
 #-dosaf 1 -minQ 35 -GL 1 \
@@ -61,43 +87,35 @@ cd /scratch/bell/mathur20/pupfish/angsd/gl/
 #-bam /scratch/bell/mathur20/pupfish/lists/allSample.bamlist \
 #-out allSample 
 
-#/depot/fnrdewoody/apps/angsd/angsd -P 64 \
-#-dosaf 1 -minQ 35 -GL 1 \
-#-anc /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
-#-bam /scratch/bell/mathur20/pupfish/lists/LR_ESU1.bamlist.txt \
-#-out LR_ESU1 
-
-#/depot/fnrdewoody/apps/angsd/angsd -P 64 \
-#-dosaf 1 -minQ 35 -GL 1 \
-#-anc /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
-#-bam /scratch/bell/mathur20/pupfish/lists/SC_ESU1.bamlist.txt \
-#-out SC_ESU1 
-
-
-#/depot/fnrdewoody/apps/angsd/angsd -P 64 \
-#-dosaf 1 -minQ 35 -GL 1 \
-#-anc /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
-#-bam /scratch/bell/mathur20/pupfish/lists/MS_ESU2.bamlist.txt \
-#-out MS_ESU2 
-
-#/depot/fnrdewoody/apps/angsd/angsd -P 64 \
-#-dosaf 1 -minQ 35 -GL 1 \
-#-anc /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
-#-bam /scratch/bell/mathur20/pupfish/lists/ESU1.bamlist.txt \
-#-out ESU1 
-
+# Get folded 1D SFS #
 
 #/depot/fnrdewoody/apps/angsd/misc/realSFS LR_ESU1.saf.idx -fold 1 -P 128 > LR_ESU1.1D.sfs
+#/depot/fnrdewoody/apps/angsd/misc/realSFS SC_ESU1.saf.idx -fold 1 -P 128 > SC_ESU1.1D.sfs
+#/depot/fnrdewoody/apps/angsd/misc/realSFS MS_ESU2.saf.idx -fold 1 -P 128 > MS_ESU2.1D.sfs
+#/depot/fnrdewoody/apps/angsd/misc/realSFS ESU1.saf.idx -fold 1 -P 128 > ESU1.1D.sfs
 
-# Get folded 2D-SFS
+# Get 3D-SFS
 
 cd /scratch/bell/mathur20/pupfish/angsd/sfs
 
 /depot/fnrdewoody/apps/angsd/misc/realSFS dadi -P 128 \
 -ref /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
 -anc /scratch/bell/mathur20/pupfish/ref/GCF_016077235.1.fna \
-LR_ESU1.saf.idx SC_ESU1.saf.idx -sfs LR_ESU1.1D.sfs -sfs SC_ESU1.1D.sfs > LR_SC.2D.sfs
+MS_ESU2.saf.idx SC_ESU1.saf.idx LR_ESU1.saf.idx -sfs MS_ESU2.1D.sfs -sfs SC_ESU1.1D.sfs -sfs LR_ESU1.1D.sfs > MS_SC_LR.3D.sfs
 
 
+# Convert 3D-sfs to dadi
+#realsfs2dadi.pl :
+#Coverts output of 'realSFS dadi' command to dadi-snp format understood by dadi and moments.
+#argument 1: input file generated with
+#realSFS dadi pop1.saf.idx pop2.saf.idx -sfs pop1.sfs -sfs pop2.sfs -ref reference.fasta -anc ancestral.fasta >dadiout
+#subsequent arguments: numbers of individuals in each population (must match the number and order of populations 
+#analyzed by realSFS)
+#prints to STDOUT
+#Example:
+#realsfs2dadi.pl dadiout 20 20 >mypops_dadi.data
 
+### ## LR (ESU1) = 15 ; SC (ESU1) = 14 ; MS (ESU2) = 15 ###
+
+/home/mathur20/2bRAD_denovo/realsfs2dadi.pl MS_SC_LR.3D.sfs  15 14 15 > MS_SC_LR.dadi.txt
 
